878917c21e2645465536baf12c100e4dcc92aea8
Ananya <ananyas237@gmail.com>
2020-10-20 23:06:39 -0400

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCb8A3k9wWKuKyqbb0GctcSBadyhaKjbRM",
+  authDomain: "phunt-clone.firebaseapp.com",
+  databaseURL: "https://phunt-clone.firebaseio.com",
+  projectId: "phunt-clone",
+  storageBucket: "phunt-clone.appspot.com",
+  messagingSenderId: "595906523859",
+  appId: "1:595906523859:web:732e9351de152eefe19a26",
+  measurementId: "G-JP0T3FZCHR"
+};
+
+export default firebaseConfig;
